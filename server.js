7f77aa2d615a4b7ca82e06b6643023ec7624d6cd
Todd Bashor <todd@thestartersacademy.com>
2014-07-16 01:41:41 -0700

    Serve real input data sample at data.json

--- server.js
+++ server.js
@@ -2,7 +2,30 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
-var data = {text: 'Hey there!'};
+var data = {
+  "id":"00000314159265359",
+  "fields":[
+    {"firstname":"Tim", "required":true},
+    {"lastname":"Cash", "required":true},
+    {"email":"tim@oakleon.com", "required":true},
+    {"Address":"12345 fake street"},
+    {"city": "San Diego"},
+    {"state": "CA"},
+    {"zipcode": "92119"},
+    {"date_created":1392164977880},
+    {"tag":"technician"},
+    {"tag":"user"},
+    {"comment":"This is the first comment about Tim"},
+    {"comment":"Here is the second comment about Tim"},
+    {"comment":"And a third comment about Tim"},
+    {"roles":{
+      "sales":false,
+      "admin":false,
+      "tech":true,
+      "manager":true
+    }}
+  ]
+};
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
