3687f63e4eccf264885816410fd61b419626f097
Todd Bashor <todd@thestartersacademy.com>
2014-07-16 01:44:57 -0700

    Put data sample in its own file to ease changes

---
+++ data.js
@@ -0,0 +1,24 @@
+module.exports = {
+  "id":"00000314159265359",
+  "fields":[
+    {"firstname":"Tim", "required":true},
+    {"lastname":"Cash", "required":true},
+    {"email":"tim@oakleon.com", "required":true},
+    {"Address":"12345 fake street"},
+    {"city": "San Diego"},
+    {"state": "CA"},
+    {"zipcode": "92119"},
+    {"date_created":1392164977880},
+    {"tag":"technician"},
+    {"tag":"user"},
+    {"comment":"This is the first comment about Tim"},
+    {"comment":"Here is the second comment about Tim"},
+    {"comment":"And a third comment about Tim"},
+    {"roles":{
+      "sales":false,
+      "admin":false,
+      "tech":true,
+      "manager":true
+    }}
+  ]
+};
